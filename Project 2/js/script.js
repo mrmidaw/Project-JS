426f8b34cee25f1f0a7f0ca8b88b629e761e50ff
mrmidaw <mr.midaw@gmail.com>
2021-03-15 11:25:35 +0300

    HomeWork Food

--- Project 2/js/script.js
+++ Project 2/js/script.js
@@ -233,29 +233,30 @@ window.addEventListener('DOMContentLoaded', function () {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function (value, key) {
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch("server.php", {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
                     statusMessage.remove();
-                    form.reset();
-                } else {
+                }).catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                }).finally(() => {
+                    form.reset();
+                });
         });
     }
 
@@ -273,6 +274,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 <div class="modal__title">${message}</div>
             </div>
         `;
+
         document.querySelector('.modal').append(thanksModal);
         setTimeout(() => {
             thanksModal.remove();
@@ -281,4 +283,15 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000);
     }
+
+
+
+
+
+
+
+
+
+
+
 });
